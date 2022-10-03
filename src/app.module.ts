840f7adfd7d889dd89c46d7291886da97c66d2c2
mubashar <hmubashar639@gmail.com>
2022-10-03 20:06:42 +0500

    main: graphql and class validation initialize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+/**
+ * App main module
+ * every mudules should
+ * register here
+ */
+
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: true,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
