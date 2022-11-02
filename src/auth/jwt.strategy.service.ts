6c8c8c9de76fdf8db58854fc5b5f73bacc635122
eqan <eqan.ahmad123@gmail.com>
2022-11-03 01:49:40 +0500

    32-authentication-and-autherization: Fixing dual jwt token authentication bug

--- src/auth/jwt.strategy.service.ts
+++ src/auth/jwt.strategy.service.ts
@@ -1,27 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import cookieParser from 'cookie-parser';
+import { Strategy } from 'passport-jwt';
+import { SystemErrors } from 'src/constants/errors.enum';
 
 // Extracts Jwt token from first the cookies and then tries to fetch it from the headers.
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private readonly configService: ConfigService) {
     super({
-      // jwtFromRequest: (req: any) =>
-      // {
-      //   try {
-      //     if (req.headers.cookies) {
-      //       const cookie = cookieParser(req.headers.cookies);
-      //       console.log(cookie)
-      //       return cookie['JWT_SECRET'];
-      //     }
-      //     return ExtractJwt.fromAuthHeaderAsBearerToken();
-      //   } catch (err) {
-      //     throw new Error(SystemErrors.COOKIES_NOT_FOUND);
-      //   }
-      // },
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (req: any) =>
+      {
+        try {
+          if (req.headers.cookies) {
+            const cookie = cookieParser(req.headers.cookies);
+            console.log("This is headers:", req.headers.cookies);
+            console.log("This is parsed: ", cookie);
+            return cookie['JWT_SECRET'];
+          }
+          const jwtToken = req.headers?.authorization;
+          if (jwtToken) {
+            return jwtToken.replace('Bearer ', '');
+          }
+        } catch (err) {
+          throw new Error(SystemErrors.COOKIES_NOT_FOUND);
+        }
+      },
+      // jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('JWT_SECRET'),
     });
