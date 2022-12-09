f8898cf1c86355f599ebde123806e83185b0ea16
nimra-tariq <nimratariq899@gmail.com>
2022-12-08 23:20:21 -0800

    87-order-match-event-filteration:match order filteration

--- src/events/handlers/order/order.handler.ts
+++ src/events/handlers/order/order.handler.ts
@@ -1,122 +1,298 @@
-import { defaultAbiCoder } from '@ethersproject/abi';
+import { defaultAbiCoder, Interface } from '@ethersproject/abi';
+import { AddressZero } from '@ethersproject/constants';
+import { getTxTrace, searchForCall } from '@georgeroman/evm-tx-simulator';
 import { Injectable, Logger } from '@nestjs/common';
+import { RpcProvider } from 'src/common/rpc-provider/rpc-provider.common';
 import { getEventData } from 'src/events/data';
 import { EnhancedEvent } from 'src/events/types/events.types';
-import { OrdersService } from 'src/orders/orders.service';
+import { Routers } from '../utils/utils.constants.order';
+import { extractAttributionData } from '../utils/utils.order';
 
 @Injectable()
 export class OrderMatchHandler {
-  constructor(private readonly ordersService: OrdersService) {}
+  constructor(
+    // private readonly ordersService: OrdersService,
+    private rpcProvider: RpcProvider,
+  ) {
+    const res =
+      Routers[1]?.['0x9757F2d2b135150BBeb65308D4a91804107cd8D6'.toLowerCase()];
+    console.log(res, 'logged res');
+  }
 
   private readonly logger = new Logger('OrderMatchHandler');
 
   handleMatchOrder = async (events: EnhancedEvent) => {
-    const { log, kind } = events;
+    const {
+      log,
+      kind,
+      baseEventParams: { txHash, address, timestamp },
+    } = events;
 
     const eventData = getEventData([kind])[0];
     try {
+      // Keep track of all events within the currently processing transaction
+      // let currentTx: string | undefined;
+      // const currentTxLogs: Log[] = [];
+      const eventsLog = {
+        matchOrders: new Map<string, number>(),
+        directPurchase: new Map<string, number>(),
+        directAcceptBid: new Map<string, number>(),
+      };
+
       const { args } = eventData.abi.parseLog(log);
-      console.log(args, 'argument logged');
       const leftHash = args['leftHash'].toLowerCase();
-      const leftMaker = args['leftMaker'].toLowerCase();
-      let taker = args['rightMaker'].toLowerCase();
-      // const newLeftFill = args['newLeftFill'].toString();
-      // const newRightFill = args['newRightFill'].toString();
-      const leftAsset = args['leftAsset'];
-      const rightAsset = args['rightAsset'];
+      const rightHash = args['rightHash'].toLowerCase();
+      const newLeftFill = args['newLeftFill'].toString();
+      const newRightFill = args['newRightFill'].toString();
 
       const ERC20 = '0x8ae85d84';
       const ETH = '0xaaaebeba';
       const ERC721 = '0x73ad2146';
       const ERC1155 = '0x973bb640';
+      const COLLECTION = '0xf63c2825';
 
-      const assetTypes = [ERC721, ERC1155, ERC20, ETH];
+      const matchOrdersSigHash = '0xe99a3f80';
+      const directPurchaseSigHash = '0x0d5f7d35';
+      const directAcceptBidSigHash = '0x67d49a3b';
+
+      const assetTypes = [ERC721, ERC1155, ERC20, ETH, COLLECTION];
+
+      const orderKind = 'rarible';
+      let side: 'sell' | 'buy' = 'sell';
+      let taker = AddressZero;
+      let currencyAssetType = '';
+      let nftAssetType = '';
+      let nftData = '';
+      let maker = '';
+      let paymentCurrency = '';
+      let amount = '';
+      let currencyPrice = '';
+      let orderId = '';
+
+      // Event data doesn't include full order information so we have to parse the calldata
+      const txTrace = await getTxTrace(
+        { hash: txHash },
+        this.rpcProvider.baseProvider,
+      );
+
+      if (!txTrace) {
+        // Skip any failed attempts to get the trace
+        return;
+      }
+
+      // Rarible has 3 fill functions: directPurchase, directAcceptBid and matchOrders.
+      // Try to parse calldata as directPurchase
+
+      try {
+        const eventRank =
+          eventsLog.directPurchase.get(`${txHash}-${address}`) ?? 0;
+
+        const callTrace = searchForCall(
+          txTrace,
+          {
+            to: address,
+            type: 'CALL',
+            sigHashes: [directPurchaseSigHash],
+          },
+          eventRank,
+        );
+        if (callTrace) {
+          const iface = new Interface([
+            'function directPurchase(tuple(address sellOrderMaker, uint256 sellOrderNftAmount, bytes4 nftAssetClass, bytes nftData, uint256 sellOrderPaymentAmount, address paymentToken, uint256 sellOrderSalt, uint sellOrderStart, uint sellOrderEnd, bytes4 sellOrderDataType, bytes sellOrderData, bytes sellOrderSignature, uint256 buyOrderPaymentAmount, uint256 buyOrderNftAmount, bytes buyOrderData))',
+          ]);
+          const result = iface.decodeFunctionData(
+            'directPurchase',
+            callTrace.input,
+          );
+          orderId = leftHash;
+          side = 'sell';
+          maker = result[0][0].toLowerCase();
+          // taker will be overwritten in extractAttributionData step if router is used
+          taker = callTrace.to.toLowerCase();
+          nftAssetType = result[0][2];
+          nftData = result[0][3];
+
+          paymentCurrency = result[0][5].toLowerCase();
+          if (paymentCurrency === AddressZero) {
+            currencyAssetType = ETH;
+          } else {
+            currencyAssetType = ERC20;
+          }
+
+          currencyPrice = newLeftFill;
+          amount = newRightFill;
+
+          eventsLog.directPurchase.set(`${txHash}-${address}`, eventRank + 1);
+        }
+      } catch {
+        // tx data doesn't match directPurchase
+      }
+
+      // Try to parse calldata as directAcceptBid
+      try {
+        const eventRank =
+          eventsLog.directAcceptBid.get(`${txHash}-${address}`) ?? 0;
+
+        const callTrace = searchForCall(
+          txTrace,
+          {
+            to: address,
+            type: 'CALL',
+            sigHashes: [directAcceptBidSigHash],
+          },
+          eventRank,
+        );
+
+        if (callTrace) {
+          const iface = new Interface([
+            'function directAcceptBid(tuple(address bidMaker, uint256 bidNftAmount, bytes4 nftAssetClass, bytes nftData, uint256 bidPaymentAmount, address paymentToken, uint256 bidSalt, uint bidStart, uint bidEnd, bytes4 bidDataType, bytes bidData, bytes bidSignature, uint256 sellOrderPaymentAmount, uint256 sellOrderNftAmount, bytes sellOrderData) )',
+          ]);
+          const result = iface.decodeFunctionData(
+            'directAcceptBid',
+            callTrace.input,
+          );
+          orderId = rightHash;
+
+          side = 'buy';
+          maker = result[0][0].toLowerCase();
+          // taker will be overwritten in extractAttributionData step if router is used
+          taker = callTrace.from.toLowerCase();
+          nftAssetType = result[0][2];
+          nftData = result[0][3];
+
+          paymentCurrency = result[0][5].toLowerCase();
+          if (paymentCurrency === AddressZero) {
+            currencyAssetType = ETH;
+          } else {
+            currencyAssetType = ERC20;
+          }
+
+          currencyPrice = newLeftFill;
+          amount = newRightFill;
+
+          eventsLog.directAcceptBid.set(`${txHash}-${address}`, eventRank + 1);
+        }
+      } catch {
+        // tx data doesn't match directAcceptBid
+      }
+
+      // Try to parse calldata as matchOrders
+      try {
+        const eventRank =
+          eventsLog.matchOrders.get(`${txHash}-${address}`) ?? 0;
+        const callTrace = searchForCall(
+          txTrace,
+          {
+            to: address,
+            type: 'CALL',
+            sigHashes: [matchOrdersSigHash],
+          },
+          eventRank,
+        );
+
+        if (callTrace) {
+          const iface = new Interface([
+            'function matchOrders(tuple(address maker, tuple(tuple(bytes4 assetClass, bytes data) assetType, uint256 value) makeAsset, address taker, tuple(tuple(bytes4 assetClass, bytes data) assetType, uint256 value) takeAsset, uint256 salt, uint256 start, uint256 end, bytes4 dataType, bytes data) orderLeft, bytes signatureLeft, tuple(address maker, tuple(tuple(bytes4 assetClass, bytes data) assetType, uint256 value) makeAsset, address taker, tuple(tuple(bytes4 assetClass, bytes data) assetType, uint256 value) takeAsset, uint256 salt, uint256 start, uint256 end, bytes4 dataType, bytes data) orderRight, bytes signatureRight)',
+          ]);
+          const result = iface.decodeFunctionData(
+            'matchOrders',
+            callTrace.input,
+          );
+          const orderLeft = result.orderLeft;
+          const orderRight = result.orderRight;
+          const leftMakeAsset = orderLeft.makeAsset;
+          const rightMakeAsset = orderLeft.takeAsset;
+
+          maker = orderLeft.maker.toLowerCase();
+          // taker will be overwritten in extractAttributionData step if router is used
+          taker = orderRight.maker.toLowerCase();
+          side = [ERC721, ERC1155].includes(leftMakeAsset.assetType.assetClass)
+            ? 'sell'
+            : 'buy';
+
+          const nftAsset = side === 'buy' ? rightMakeAsset : leftMakeAsset;
+          const currencyAsset = side === 'buy' ? leftMakeAsset : rightMakeAsset;
+
+          orderId = leftHash;
+          nftAssetType = nftAsset.assetType.assetClass;
+          currencyAssetType = currencyAsset.assetType.assetClass;
+          switch (nftAssetType) {
+            case COLLECTION:
+              // Left order doesn't contain token id. We need to use the right order
+              nftData = orderRight.makeAsset.assetType.data;
+              break;
+            case ERC721:
+            case ERC1155:
+              nftData = nftAsset.assetType.data;
+              break;
+            default:
+              throw Error('Unsupported asset type');
+          }
+
+          if (currencyAssetType === ETH) {
+            paymentCurrency = 'ETH';
+          } else if (currencyAssetType === ERC20) {
+            const decodedCurrencyAsset = defaultAbiCoder.decode(
+              ['(address token)'],
+              currencyAsset.assetType.data,
+            );
+            paymentCurrency = decodedCurrencyAsset[0][0].toLowerCase();
+          }
+
+          // Match order has amount in newLeftFill when it's a buy order and amount in newRightFill when it's sell order
+          amount = side === 'buy' ? newLeftFill : newRightFill;
+          currencyPrice = side === 'buy' ? newRightFill : newLeftFill;
+
+          eventsLog.matchOrders.set(`${txHash}-${address}`, eventRank + 1);
+        }
+      } catch {
+        // tx data doesn't match matchOrders
+      }
 
       // Exclude orders with exotic asset types
       if (
-        !assetTypes.includes(leftAsset.assetClass) ||
-        !assetTypes.includes(rightAsset.assetClass)
+        !assetTypes.includes(nftAssetType) ||
+        !assetTypes.includes(currencyAssetType)
       ) {
         return;
       }
 
-      // Assume the left order is the maker's order
-      const side = [ERC721, ERC1155].includes(leftAsset.assetClass)
-        ? 'sell'
-        : 'buy';
-
-      // const currencyAsset = side === 'sell' ? rightAsset : leftAsset;
-      const nftAsset = side === 'sell' ? leftAsset : rightAsset;
-
-      // Handle: attribution
-
-      const orderKind = eventData.kind.startsWith('universe')
-        ? 'universe'
-        : 'rarible';
-      const data = { taker: '88' };
-      // await syncEventsUtils.extractAttributionData(
-      //   txHash,
-      //   orderKind,
-      // );
-      if (data.taker) {
-        taker = data.taker;
+      // Handle: attribution;
+      const attributionData = await extractAttributionData(
+        txHash,
+        this.rpcProvider.baseProvider,
+      );
+      console.log(attributionData, 'hello attribution logged oout ');
+      if (attributionData.taker) {
+        console.log(attributionData.taker, 'taker logged');
+        taker = attributionData.taker;
       }
-
-      // Handle: prices
-
-      // let currency: string;
-      // if (currencyAsset.assetClass === ETH) {
-      //   currency = Sdk.Common.Addresses.Eth[config.chainId];
-      // } else if (currencyAsset.assetClass === ERC20) {
-      //   const decodedCurrencyAsset = defaultAbiCoder.decode(
-      //     ['(address token)'],
-      //     currencyAsset.data,
-      //   );
-      //   currency = decodedCurrencyAsset[0][0];
-      // } else {
-      //   break;
-      // }
 
       const decodedNftAsset = defaultAbiCoder.decode(
         ['(address token, uint tokenId)'],
-        nftAsset.data,
+        nftData,
       );
-
       const contract = decodedNftAsset[0][0].toLowerCase();
       const tokenId = decodedNftAsset[0][1].toString();
 
-      // let currencyPrice = side === 'sell' ? newLeftFill : newRightFill;
-      // const amount = side === 'sell' ? newRightFill : newLeftFill;
-      // currencyPrice = bn(currencyPrice).div(amount).toString();
+      const data = {
+        orderKind,
+        orderId,
+        orderSide: side,
+        maker,
+        taker,
+        // price: prices.nativePrice,
+        // currency,
+        currencyPrice,
+        // usdPrice: prices.usdPrice,
+        contract,
+        tokenId,
+        amount,
+        timestamp,
+        txHash,
+      };
 
-      // const prices = await getUSDAndNativePrices(
-      //   currency.toLowerCase(),
-      //   currencyPrice,
-      //   timestamp,
-      // );
-      // if (!prices.nativePrice) {
-      //   // We must always have the native price
-      //   break;
-      // }
-      // const returnData = {
-      //   orderKind,
-      //   orderId: leftHash,
-      //   orderSide: side,
-      //   maker: leftMaker,
-      //   taker,
-      //   // price: prices.nativePrice,
-      //   // currency,
-      //   // currencyPrice,
-      //   // usdPrice: prices.usdPrice,
-      //   contract,
-      //   tokenId,
-      //   baseEventParams,
-      //   // amount,
-      //   // orderSourceId: data.orderSource?.id,
-      //   // aggregatorSourceId: data.aggregatorSource?.id,
-      //   // fillSourceId: data.fillSource?.id,
-      //   // baseEventParams,
-      // };
+      console.log(data, 'data logged out after listening event');
     } catch (error) {
       this.logger.error(`failed Matching Order ${error}`);
     }
@@ -134,6 +310,7 @@ export class OrderMatchHandler {
         orderId,
         baseEventParams,
       };
+      console.log(returnData, 'cancel event return data');
     } catch (error) {
       this.logger.error(`failed Cancelling Order ${error}`);
     }
---
+++ src/events/handlers/utils/utils.constants.order.ts
@@ -0,0 +1,39 @@
+export type ChainIdToAddressMap = {
+  [chainId: number]: { [address: string]: string };
+};
+
+export const Routers: ChainIdToAddressMap = {
+  // Network.Ethereum
+  1: {
+    // Rarible
+    '0x9757f2d2b135150bbeb65308d4a91804107cd8d6': 'rarible.com',
+    '0x2a7251d1e7d708c507b1b0d3ff328007beecce5d': 'rarible.com',
+    '0x7f19564c35c681099c0c857a7141836cf7edaa53': 'rarible.com',
+    // Reservoir (modules)
+    '0x920692834f93258b71221c58edf870ae013e2f9b': 'reservoir.tools',
+    '0xef21d6b43ac0bb4608ca05628b05403a47310a3b': 'reservoir.tools',
+    '0xff78f7c6e23187fd4bdb2f7f35359a42d56878dd': 'reservoir.tools',
+    '0x5c8a351d4ff680203e05af56cb9d748898c7b39a': 'reservoir.tools',
+    '0x385df8cbc196f5f780367f3cdc96af072a916f7e': 'reservoir.tools',
+    '0x3729014ef28f01b3ddcf7f980d925e0b71b1f847': 'reservoir.tools',
+    '0xecd3184bc21172ea96061afeefaa136e5b3761b6': 'reservoir.tools',
+    '0x613d3c588f6b8f89302b463f8f19f7241b2857e2': 'reservoir.tools',
+    '0x8162beec776442afd262b672730bb5d0d8af16a1': 'reservoir.tools',
+    '0x982b49de82a3ea5b8c42895482d9dd9bfefadf82': 'reservoir.tools',
+    '0xb1096516fc33bb64a77158b10f155846e74bd7fa': 'reservoir.tools',
+  },
+  //   Network.EthereumGoerli
+  5: {
+    // Reservoir (routers)
+    '0xf44caa746d184e6fba3071e8adbf9c041620fe44': 'reservoir.tools',
+    '0xb35d22a4553ab9d2b85e2a606cbae55f844df50c': 'reservoir.tools',
+    // Reservoir (modules)
+    '0xe4c1c635f257348205ebca78fc9b342dd7813e2b': 'reservoir.tools',
+    '0x037d39e603b803651acc7b36ff25e52f8680aa2f': 'reservoir.tools',
+    '0x0e01862920bd5ef73ed1a5dccd2ecad56c3e051f': 'reservoir.tools',
+    '0x532486bb46581b032134159c1d31962cdab1e6a7': 'reservoir.tools',
+    '0x6c460f133c573c21e7f55900d0c68f6f085b91e7': 'reservoir.tools',
+    '0x6a789513b2e555f9d3539bf9a053a57d2bfca426': 'reservoir.tools',
+    '0x29fcac61d9b2a3c55f3e1149d0278126c31abe74': 'reservoir.tools',
+  },
+};
