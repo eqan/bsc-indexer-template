c0f7e2bcc6729307aacca74d13b7a2dbcf80ca50
nimra-tariq <nimratariq899@gmail.com>
2022-12-27 04:08:03 -0800

    115-on-chain-order-storage-in-local-database:enhanced assetTypes

--- src/orders/constants/orders.constants.validation-schema.ts
+++ src/orders/constants/orders.constants.validation-schema.ts
@@ -117,6 +117,33 @@ export const assetTypeValidationSchema = {
       additionalProperties: false,
       required: ['contract', 'tokenId'],
     },
+    {
+      properties: {
+        assetClass: { enum: ['ERC721_LAZY'] },
+        contract: { type: 'string' },
+        tokenId: { type: 'string' },
+        uri: { type: 'string' },
+        creators: { type: 'array', items: Part },
+        royalties: { type: 'array', items: Part },
+        signatures: { types: 'array', items: { type: 'string' } },
+      },
+      additionalProperties: false,
+      required: ['contract', 'tokenId', 'uri', 'signatures'],
+    },
+    {
+      properties: {
+        assetClass: { enum: ['ERC1155_LAZY'] },
+        contract: { type: 'string' },
+        tokenId: { type: 'string' },
+        uri: { type: 'string' },
+        supply: { type: 'number' },
+        creators: { type: 'array', items: Part },
+        royalties: { type: 'array', items: Part },
+        signatures: { types: 'array', items: { type: 'string' } },
+      },
+      additionalProperties: false,
+      required: ['contract', 'tokenId', 'uri', 'supply', 'signatures'],
+    },
   ],
 };
 
