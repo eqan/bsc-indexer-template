433187c2f2496615b6485acec4c454244150604f
eqan <eqan.ahmad123@gmail.com>
2023-01-07 16:51:38 +0500

    136-cron-job-based-module-to-calculate-day-volume-and-floor-price: Adding Cron expression

--- src/stats/stats.resolver.ts
+++ src/stats/stats.resolver.ts
@@ -5,7 +5,7 @@ import { FilterStatsDto } from './dto/filter-stats.dto';
 import { GetAllStats } from './dto/get-all-stats.dto';
 import { Stats } from './entities/stats.entity';
 import { CollectionsResolver } from 'src/collections/collections.resolver';
-import { Cron, SchedulerRegistry } from '@nestjs/schedule';
+import { Cron, CronExpression, SchedulerRegistry } from '@nestjs/schedule';
 import { CronType } from 'src/jobs/types/cron.types';
 import { QueueType } from 'src/jobs/enums/jobs.enums';
 
@@ -19,7 +19,7 @@ export class StatsResolver {
   CRON_NAME = QueueType.STATS_CRON;
 
   @Mutation(() => Stats, { name: 'CreateUpdateStats', nullable: true })
-  @Cron('0 0 * * *', { name: QueueType.STATS_CRON })
+  @Cron(CronExpression.EVERY_12_HOURS, { name: QueueType.STATS_CRON })
   async create(): Promise<void> {
     const { items } = await this.collectionsResolver.index({
       page: 0,
